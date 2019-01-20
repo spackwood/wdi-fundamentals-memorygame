b38677e216552518001c685125654c5abb10fee4
Spackwood <sean.packwood@gmail.com>
2019-01-20 13:18:58 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+var cardOne = "king";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "queen";
+
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardTwo);
+console.log("user flipped " + cardThree);
+console.log("user flipped " + cardFour);
