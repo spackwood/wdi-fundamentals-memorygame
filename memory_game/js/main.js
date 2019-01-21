27f80c4a6310817c5de0f42fb4f09569693226cf
Spackwood <sean.packwood@gmail.com>
2019-01-21 12:04:37 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,17 @@
 console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
+console.log("user flipped " + "cardOne");
+console.log("user flipped " + "cardTwo");
+console.log("user flipped " + "cardThree");
+console.log("user flipped " + "cardFour");
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardTwo);
-console.log("user flipped " + cardThree);
-console.log("user flipped " + cardFour);
+var cards = ["cardOne", "cardTwo", "cardThree", "cardFour"];
+var cardsInPlay = ["cardOne", "cardTwo"];
+
+console.log("User flipped queen");
+console.log("user flipped king");
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	result = alert("You found a match!");
+} else {
+	result = alert("Try again!");
+}
